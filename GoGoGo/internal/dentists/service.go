940d8f92a927392df37863b76ff1f5fad2e2d4f6
bedulem <santochibelen@gmail.com>
2023-09-16 18:05:00 -0300

    servicios comentados

--- GoGoGo/internal/dentists/service.go
+++ GoGoGo/internal/dentists/service.go
@@ -5,10 +5,10 @@ package dentists
 
 
 type Repository interface {
-	Save(dentist Dentist)(Dentist, error)
+	//Save(dentist Dentist)(Dentist, error)
 	GetByID(id int) (Dentist, error)
-	Modify(id int, dentist Dentist) (Dentist, error)
-	Delete(id int)(error)
+	//Modify(id int, dentist Dentist) (Dentist, error)
+	//Delete(id int)(error)
 }
 
 // Service provides all functionalities related to dentist.
@@ -20,18 +20,18 @@ func NewService(repository Repository) *Service {
 	return &Service{repository: repository}
 }
 
-func (s *Service) Save(dentist Dentist) (Dentist, error) {
-	return s.repository.Save(dentist)
-}
+// func (s *Service) Save(dentist Dentist) (Dentist, error) {
+// 	return s.repository.Save(dentist)
+// }
 
 func (s *Service) GetByID(id int) (Dentist, error) {
 	return s.repository.GetByID(id)
 }
 
-func (s *Service) ModifyByID(id int, dentist Dentist) (Dentist, error) {
-	return s.repository.Modify(id, dentist)
-}
+// func (s *Service) ModifyByID(id int, dentist Dentist) (Dentist, error) {
+// 	return s.repository.Modify(id, dentist)
+// }
 
-func (s *Service) Delete(id int)(error) {
-	return s.repository.Delete(id)
-}
+// func (s *Service) Delete(id int)(error) {
+// 	return s.repository.Delete(id)
+// }
